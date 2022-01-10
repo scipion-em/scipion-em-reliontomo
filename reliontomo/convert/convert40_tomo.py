bec5123f49639c5c3d4dbca93ae8b4224fb7b899
JorMaister <jorgejjs86@gmail.com>
2022-01-10 16:53:49 +0100

    Fix sifts and angles initialization in Reader

--- reliontomo/convert/convert40_tomo.py
+++ reliontomo/convert/convert40_tomo.py
@@ -247,8 +247,8 @@ class Reader:
 
     def __init__(self, **kwargs):
         # self.setParticleTransform = None
-        self._shifts = None
-        self._angles = None
+        self._shifts = [0, 0, 0]
+        self._angles = [0, 0, 0]
         self._alignType = kwargs.get('alignType', ALIGN_NONE)
         self._pixelSize = kwargs.get('pixelSize', 1.0)
 
