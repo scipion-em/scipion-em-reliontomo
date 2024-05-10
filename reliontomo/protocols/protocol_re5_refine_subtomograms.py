ba0d4a06de7804db52885b2c5b49924e4e5af59f
JorMaister <jorgejjs86@gmail.com>
2024-05-10 11:02:00 +0200

    Refine --> resolve the halves properly for the reference

--- reliontomo/protocols/protocol_re5_refine_subtomograms.py
+++ reliontomo/protocols/protocol_re5_refine_subtomograms.py
@@ -287,7 +287,7 @@ class ProtRelion5RefineSubtomograms(ProtRelion5RefineBase):
         # If halves exists, use a half as explained here:
         # https://relion.readthedocs.io/en/release-4.0/STA_tutorial/Refine3D.html#high-resolution-3d-refinement
         if ref.hasHalfMaps():
-            refFile = ref.getHalfMaps()[0]
+            refFile, _ = ref.getHalfMaps()
 
         cmd += '--ref %s ' % refFile
         if self.solventMask.get():
