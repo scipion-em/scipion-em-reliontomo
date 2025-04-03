318d888f933c6fa55b0083e237b48fc68a8bcae5
JorMaister <jorgejjs86@gmail.com>
2025-04-03 12:14:56 +0200

    Bump version

--- reliontomo/__init__.py
+++ reliontomo/__init__.py
@@ -24,7 +24,7 @@
 # **************************************************************************
 _logo = "relion_logo.jpg"
 _references = ['Zivanov2022', 'Burt2024']
-__version__ = '4.0.1'
+__version__ = '4.0.2'
 
 try:
     import pwem
