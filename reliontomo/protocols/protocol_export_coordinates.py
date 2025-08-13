fe994f20e1d8110071d2f76150c795b994b8366a
JorMaister <jorgejjs86@gmail.com>
2025-08-13 18:31:53 +0200

    Add export coords

--- reliontomo/protocols/protocol_export_coordinates.py
+++ reliontomo/protocols/protocol_export_coordinates.py
@@ -90,18 +90,6 @@ class ProtTomoExportRe5Coords(EMProtocol):
 
     # --------------------------- INSERT steps functions ----------------------
     def _insertAllSteps(self):
-        # JORGE
-        import os
-        fname = "/home/jjimenez/test_JJ.txt"
-        if os.path.exists(fname):
-            os.remove(fname)
-        fjj = open(fname, "a+")
-        fjj.write('JORGE--------->onDebugMode PID {}'.format(os.getpid()))
-        fjj.close()
-        print('JORGE--------->onDebugMode PID {}'.format(os.getpid()))
-        import time
-        time.sleep(10)
-        # JORGE_END
         self._initialize()
         self._insertFunctionStep(self.exportCoordsStep, needsGPU=False)
 
